77c42f1e844da6fa11820470561260e71f074197
gpownall <gavin.pownall@cern.ch>
2020-12-24 11:39:17 +0000

    Model running

--- src/Models/LSTM_NN.py
+++ src/Models/LSTM_NN.py
@@ -9,6 +9,48 @@ data = Data()
 class LSTM(nn.Module):
     def __init__(self):
         super().__init__()
+        ## Sizes of various things
         self.hidden_layer_size = 100
-        self.lstm = nn.LSTM(1, self.hidden_layer_size)
-        
+        n_features = data.n_features()
+        output_size = 1
+
+        ## layers
+        self.lstm = nn.LSTM(6, self.hidden_layer_size)
+        self.linear = nn.Linear(self.hidden_layer_size, output_size)
+        self.reset_hidden_cell()
+
+    def reset_hidden_cell(self):
+        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                            torch.zeros(1,1,self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        ## Define function to calculate predictions
+
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq),1,-1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq),-1))
+        return predictions[-1]
+
+
+def train():
+    model = LSTM()
+    loss_function = nn.MSELoss()
+    optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    print(model)
+    
+    epochs = 10
+    train_x, train_y, test_x, test_y = data.train_test()
+
+    for i in range(epochs):
+        for x,y in zip(train_x, train_y):
+            optimiser.zero_grad()
+            model.reset_hidden_cell()
+            y_pred = model(torch.FloatTensor(x))
+
+            single_loss = loss_function(y_pred, torch.FloatTensor([y]))
+            single_loss.backward()
+            optimiser.step()
+
+        print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+
+train()
--- src/Utils/load_data.py
+++ src/Utils/load_data.py
@@ -1,10 +1,63 @@
 import datatable as dt
+import numpy as np
+
+def create_inout_sequences(input_vector, seq_len):
+    out_seq = []
+    L = len(input_vector)
+    for i in range(L-seq_len):
+        x = input_vector[i:i+seq_len+1]
+        out_seq.append(x)
+    #print(input_vector)
+    #print(out_seq)
+    return out_seq
 
 class Data:
     def __init__(self, short=True):
-        if short: self.df = df = dt.fread('input/train.csv',max_nrows=1000).to_pandas()
+        if short: self.df = df = dt.fread('input/train.csv',max_nrows=50).to_pandas()
         else: self.df = dt.fread('input/train.csv').to_pandas()
+        self.df.drop(['feature_'+str(i) for i in range(2,130)]+['resp_'+str(i) for i in range(1,5)],axis=1, inplace=True)
 
+    def n_features(self):
+        features = [x for x in self.df.columns if 'feature' in x]
+        return len(features)
+
+    def features_as_np(self):
+        return [self.df[x].values.astype(float) for x in self.df.columns if 'feature' in x]
+
+    def train_test(self, train_frac = 0.7, seq_len=5):
+        x = self.features_as_np()
+        y = self.df.resp
+        train_n = int(train_frac*len(self.df))
+        test_n  = len(self.df) - train_n
+
+        train_x = []
+        test_x  = []
+        for i in range(len(x)):
+            x[i] = np.concatenate([np.zeros(seq_len), x[i]])
+            #print(x[i])
+            train_x.append(create_inout_sequences(x[i][:train_n+seq_len],seq_len))
+            test_x .append(create_inout_sequences(x[i][train_n:],seq_len))
+
+        train_y = y[:train_n].values.astype(float)
+        test_y  = y[train_n:].values.astype(float)
+
+        return train_x, train_y, test_x, test_y
+        
+
+if __name__ == '__main__':
+    x = Data()
+    print(x.df)
+    #print('===============')
+    #print (x.features_as_np())
+    print('==================')
+    for n,i in enumerate(x.train_test()):
+        #if n in [0,1]: continue
+        print('\n')
+        print(i)
+        if n in [0,2]: 
+            print ([len(j) for j in i])
+        else: 
+            print (len(i))
 
 '''
 df = dt.fread('input/train.csv').to_pandas()
