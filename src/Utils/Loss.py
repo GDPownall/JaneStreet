800ee1b943b9b58213951fd44091a29e81ea9d62
gpownall <gavin.pownall@cern.ch>
2021-01-07 17:16:24 +0000

    Round action in loss function

--- src/Utils/Loss.py
+++ src/Utils/Loss.py
@@ -6,5 +6,6 @@ Contains custom loss functions
 import torch
 
 def custom_loss(action, resp, weight):
-    loss = torch.sum(-1*action*resp*weight)
+    rounded = torch.round(action)
+    loss = torch.sum(-1*rounded*resp*weight)
     return loss
