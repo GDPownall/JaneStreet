4b6b60a44e2aeab7262a34b2182ae82bd1a6a5ee
gpownall <gavin.pownall@cern.ch>
2021-01-08 10:06:34 +0000

    Abs params

--- src/Utils/Loss.py
+++ src/Utils/Loss.py
@@ -8,6 +8,6 @@ import torch
 def custom_loss(action, resp, weight, regularise=None):
     loss = torch.sum(-1*action*resp*weight)
     if regularise != None:
-        loss += torch.sum(regularise)
+        loss += torch.sum(torch.abs(regularise))
     #if regularise != None: print(torch.sum(-1*action*resp*weight),torch.sum(torch.abs(regularise)))
     return loss
