fb376f879814f27eb9a66e8fce1d2a6d467445ac
gpownall <gavin.pownall@cern.ch>
2021-01-08 09:15:02 +0000

    Fix: use absolute values of parameters

--- src/Utils/Loss.py
+++ src/Utils/Loss.py
@@ -9,5 +9,5 @@ def custom_loss(action, resp, weight, regularise=None):
     loss = torch.sum(-1*action*resp*weight)
     if regularise != None:
         loss += torch.sum(regularise)
-    if regularise != None: print(torch.sum(-1*action*resp*weight),torch.sum(regularise))
+    if regularise != None: print(torch.sum(-1*action*resp*weight),torch.sum(torch.abs(regularise)))
     return loss
